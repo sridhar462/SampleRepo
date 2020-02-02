5c8a688affad04a72abcc4d26d1702244b0874c0
sridhar462 <sridhar462@gmail.com>
2020-02-03 00:51:03 +0530

    first commit

---
+++ src/main/java/com/learnfromexamples/SampleRepo/App.java
@@ -0,0 +1,13 @@
+package com.learnfromexamples.SampleRepo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
